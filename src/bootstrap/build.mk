98f9d0dfa693a50ed436122cacd3f9ccda2c25e8
Lance R. Vick <lance@lrvick.net>
2024-01-17 00:46:02 -0800

    add WIP cross-x86_64

--- src/bootstrap/build.mk
+++ src/bootstrap/build.mk
@@ -1,10 +1,14 @@
 .PHONY: bootstrap
 bootstrap: \
-	out/bootstrap.tgz \
-	out/stage0.tgz
-
-out/bootstrap.tgz: out/stage0.tgz
-	$(call build,bootstrap,bootstrap)
+	out/live-bootstrap.tgz \
+	out/stage0.tgz \
+	out/cross-x86_64.tgz
 
 out/stage0.tgz:
 	$(call build,bootstrap,stage0)
+
+out/live-bootstrap.tgz: out/stage0.tgz
+	$(call build,bootstrap,live-bootstrap)
+
+out/cross-x86_64.tgz: out/live-bootstrap.tgz
+	$(call build,bootstrap,cross-x86_64)
