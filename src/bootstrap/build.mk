016a9d4c5c11171ba5b6937c8aa7a6ab5355b97e
Lance R. Vick <lance@lrvick.net>
2024-01-21 06:03:20 -0800

    refactor bootstrap as stages 0-2

--- src/bootstrap/build.mk
+++ src/bootstrap/build.mk
@@ -1,14 +1,14 @@
 .PHONY: bootstrap
 bootstrap: \
-	out/live-bootstrap.tgz \
 	out/stage0.tgz \
-	out/cross-x86_64.tgz
+	out/stage1.tgz \
+	out/stage2.tgz
 
 out/stage0.tgz:
 	$(call build,bootstrap,stage0)
 
-out/live-bootstrap.tgz: out/stage0.tgz
-	$(call build,bootstrap,live-bootstrap)
+out/stage1.tgz: out/stage0.tgz
+	$(call build,bootstrap,stage1)
 
-out/cross-x86_64.tgz: out/live-bootstrap.tgz
-	$(call build,bootstrap,cross-x86_64)
+out/stage2.tgz: out/stage1.tgz
+	$(call build,bootstrap,stage2)
