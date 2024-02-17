66a5605f206aad97833611d3fc38783558a9bc58
Lance R. Vick <lance@lrvick.net>
2024-02-17 10:19:47 -0800

    feat: preseed support

---
+++ src/preseed.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+fetch(){
+	line=${1?}
+	local package=$(echo ${line} | sed 's/^[a-z0-9]\+ \(.*\)/\1/g');
+	local hash=$(echo ${line} | sed 's/^\([a-z0-9]\+\) .*/\1/g');
+	local ref=$(printf "stagex/%s@sha256:%s" "$package" "$hash");
+	docker pull ${ref}
+	rm -rf "out/${package}"
+	mkdir -p "out/${package}"
+	docker save ${ref} | tar -xC out/${package}
+}
+
+while read line; do
+	fetch "${line}"
+done < digests.txt
