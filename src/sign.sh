dfad7b6af1e4f83ea59beaf986815883e993e0a7
ryan <ryan@distrust.co>
2024-03-31 22:31:01 -0400

    src/sign: improve handling adding new signatures

--- src/sign.sh
+++ src/sign.sh
@@ -12,16 +12,47 @@ SIGNUM=1
 
 mkdir -p ${DIR}
 
-[ -f ${DIR}/signature-1 ] \
-    && LASTSIGNUM=$( \
-        find ${DIR} -type f -printf "%f\n" \
-        | sort -t- -k2 -n \
-        | tail -n1 \
-        | sed 's/signature-//' \
-    ) \
-    && let "SIGNUM=LASTSIGNUM+1"
+get-filename() {
+  DIR="$1"
+  SIGNUM=1
+  [ -f "${DIR}/signature-1" ] \
+      && LASTSIGNUM=$( \
+          find ${DIR} -type f -printf "%f\n" \
+          | sort -t- -k2 -n \
+          | tail -n1 \
+          | sed 's/signature-//' \
+      ) \
+      && let "SIGNUM=LASTSIGNUM+1"
+  echo "${DIR}/signature-${SIGNUM}"
+}
 
-printf \
-    '[{"critical":{"identity":{"docker-reference":"%s/%s"},"image":{"docker-manifest-digest":"%s"},"type":"pgp container image signature"},"optional":null}]' \
-    "$REGISTRY" "$NAME" "$ID" \
-    | gpg --sign > ${DIR}/signature-${SIGNUM}
+dir-has-no-sig() {
+  echo "$DIR" >/dev/stderr
+  DIR="$1"
+  FP="$2"
+  for file in "${DIR}"/*; do
+    # We want to check if a fingerprint matches, we don't need to check if
+    # the signature is valid.
+    if (gpgv "$file" >/dev/null || :) 2>&1 | grep "$FP"; then
+      echo "found matching signature: $file" >/dev/stderr
+      return 1
+    fi
+  done
+  return 0
+}
+
+SIGNING_KEY="$(git config user.signingkey)"
+FPR="$(gpg --list-keys --with-colons "$SIGNING_KEY" | grep fpr | cut -d: -f10 | head -n1)"
+test ! -z "$FPR"
+
+TEMPFILE="$(mktemp)"
+
+if dir-has-no-sig "$DIR" "$FPR"; then
+  echo "Signing: $NAME"
+  FILENAME="$(get-filename "$DIR")"
+  printf \
+      '[{"critical":{"identity":{"docker-reference":"%s/%s"},"image":{"docker-manifest-digest":"%s"},"type":"pgp container image signature"},"optional":null}]' \
+      "$REGISTRY" "$NAME" "$ID" \
+      | gpg --sign > "$TEMPFILE"
+  mv "$TEMPFILE" "$FILENAME"
+fi
