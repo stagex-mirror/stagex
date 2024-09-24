098232294b34ce89689fc3c77b3d628e6d28eea7
ryan <ryan@distrust.co>
2024-09-23 22:23:43 -0400

    rust-deps: actually run across all images

--- src/rust-deps-stats/rust-deps.sh
+++ src/rust-deps-stats/rust-deps.sh
@@ -2,8 +2,7 @@
 
 SCRIPTDIR="$(cd "$(dirname $0)"; pwd)"
 
-# for distro in debian:bookworm debian:bookworm-slim archlinux:base fedora:40 alpine:3.20.3 nixos/nix:2.24.7; do
-for distro in nixos/nix:2.24.7; do
+for distro in debian:bookworm debian:bookworm-slim archlinux:base fedora:40 alpine:3.20.3 nixos/nix:2.24.7; do
   script="$(echo "${distro}" | tr "/" "_")"
   docker run --rm -v "$SCRIPTDIR:/scripts:ro" $distro /bin/sh /scripts/$script.sh | grep --color "^DEPS"
 done
