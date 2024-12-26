2a57a1794a25c387328b53d9626b6bae0e95460d
Jakub Panek <me@panekj.dev>
2024-12-26 09:10:41 +0100

    format python code with black

--- src/digests.py
+++ src/digests.py
@@ -6,22 +6,22 @@ from os import mkdir
 from shutil import rmtree
 
 digests = {}
-for filename in glob('out/**/index.json',recursive=True):
-  fullname = filename.split('/')[1]
-  stage = fullname.split('-')[0]
-  name = '-'.join(fullname.split('-')[1:])
-  if stage not in digests:
-    digests[stage] = []
-  with open(filename) as file:
-    data = json.load(file)
-  digest = data['manifests'][0]['digest'].split(":")[1]
-  digests[stage].append((name,digest))
+for filename in glob("out/**/index.json", recursive=True):
+    fullname = filename.split("/")[1]
+    stage = fullname.split("-")[0]
+    name = "-".join(fullname.split("-")[1:])
+    if stage not in digests:
+        digests[stage] = []
+    with open(filename) as file:
+        data = json.load(file)
+    digest = data["manifests"][0]["digest"].split(":")[1]
+    digests[stage].append((name, digest))
 
-rmtree('digests')
-mkdir('digests')
-for stage,elements in digests.items():
-  filename = "digests/%s.txt" % stage
-  with open(filename, 'a') as file:
-    for element in sorted(elements,key=operator.itemgetter(0)):
-      file.write("%s %s-%s\n" % (element[1],stage,element[0]))
-  print("> %s" % filename),
+rmtree("digests")
+mkdir("digests")
+for stage, elements in digests.items():
+    filename = "digests/%s.txt" % stage
+    with open(filename, "a") as file:
+        for element in sorted(elements, key=operator.itemgetter(0)):
+            file.write("%s %s-%s\n" % (element[1], stage, element[0]))
+    print("> %s" % filename),
