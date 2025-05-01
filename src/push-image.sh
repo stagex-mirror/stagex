8ee6e4bc9f899e262ccf8b1618a5ab689a7bda4f
ryan <ryan@distrust.co>
2025-05-01 02:45:16 -0400

    push inside the hash checker

--- src/push-image.sh
+++ src/push-image.sh
@@ -5,11 +5,22 @@
 # If the program exits successfully, the manifest exists, and does not need to be pushed.
 # USAGE: sh src/has-manifest.sh stagex/bootstrap-stage0:sx2025.04.0 ./out/bootstrap-stage0
 
+# NOTE: Do not set pipefail to maintain POSIX compatibility, but also to ensure
+# not having a manifest on the remote doesn't result in an exit.
 set -eux
 REMOTE="$1"
 LOCAL="$2"
 
 manifest_digest_file="$(jq -er '.manifests[0].digest' "$LOCAL/index.json" | tr ':' '/')"
 manifest_digest="$(jq -er '.manifests[0].digest' "$LOCAL/blobs/$manifest_digest_file")"
-remote_digest="$(docker manifest inspect "$REMOTE" | jq -er '.manifests[0].digest')"
-test "$manifest_digest" = "$remote_digest"
+# fail-safe: if the command fails, and jq can't make valid output, it will not
+# compare to the local image, and we will continue to the push stage.
+remote_digest="$(docker manifest inspect "$REMOTE" | jq -r '.manifests[0].digest')"
+
+if test ! "$manifest_digest" = "$remote_digest"; then
+  # assume the container is tagged and ready to push
+  echo "Pushing... $REMOTE"
+  while ! docker push "$REMOTE"; do
+    echo "Retrying... $REMOTE"
+  done
+fi
